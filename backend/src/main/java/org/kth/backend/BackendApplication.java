08258e5004a13ac45175393705b88a4f7d614a8c
Ackuq@ <axel@pettersson.cc>
2020-12-01 14:47:02 +0100

    Ajust adjust configurations

--- backend/src/main/java/org/kth/backend/BackendApplication.java
+++ backend/src/main/java/org/kth/backend/BackendApplication.java
@@ -1,13 +1,19 @@
 package org.kth.backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackendApplication {
+
+	@Value("${ENV:dev}")
+	private String ENV;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
@@ -18,8 +24,20 @@ public class BackendApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowCredentials(true);
+
+				registry.addMapping("/**")
+						.allowedOrigins("https://id1212-project-frontend.herokuapp.com", "http://localhost:3000")
+						.allowCredentials(true);
 			}
 		};
+	}
+
+	@Bean
+	public CookieSerializer cookieSerializer() {
+		DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+		if (ENV.equals("prod")) {
+			serializer.setSameSite("None");
+		}
+		return serializer;
 	}
 }
