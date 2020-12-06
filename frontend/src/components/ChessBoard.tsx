4d5acc56aa7543d82b289da7845e5f84392c483f
Ackuq@ <axel@pettersson.cc>
2020-12-06 21:24:30 +0100

    Add move highlightning

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,13 +1,8 @@
 import { styled, useTheme } from '@material-ui/core';
 import { BreakpointValues } from '@material-ui/core/styles/createBreakpoints';
 import Chessboard from 'chessboardjsx';
-import { useContext, useEffect, useRef, useState } from 'react';
-import { Message } from 'stompjs';
+import { useContext } from 'react';
 import ChessContext from '../context/ChessContext';
-import SnackbarContext from './SnackBar';
-import Stomp from 'stompjs';
-import AuthContext from '../context/AuthContext';
-import Loading from './Loading';
 
 const SMALL_SCREEN_PADDING = 100;
 const SMALL_SCREEN_HEIGHT_PADDING = 100;
@@ -25,72 +20,25 @@ const calcWidth = (breakpoints: BreakpointValues): typeof Chessboard.prototype.p
   return Math.min(screenWidth - LARGE_SCREEN_PADDING, screenHeight - LARGE_SCREEN_HEIGHT_PADDING);
 };
 
-const baseWSUrl = process.env.REACT_APP_WS_URL || 'ws://localhost:8080';
-
 const ChessBoard: React.FC = () => {
   const theme = useTheme();
 
-  const [myTurn, setMyTurn] = useState(false);
-  const [myColor, setColor] = useState('');
-  const { code, chessboard, updateGame } = useContext(ChessContext);
-  const { email } = useContext(AuthContext);
-  const openSnackbar = useContext(SnackbarContext);
-  const stompClient = useRef(Stomp.over(new WebSocket(`${baseWSUrl}/connect`)));
+  const { onDrop, onMouseOverSquare, onMouseOutSquare, position, me, squareStyles } = useContext(
+    ChessContext
+  );
 
-  useEffect(() => {
-    const socket = new WebSocket(`${baseWSUrl}/connect`);
-    stompClient.current = Stomp.over(socket);
-
-    stompClient.current.connect({ user: email }, (frame) => {
-      stompClient.current.subscribe(`/chess/${code}`, handleMove);
-      stompClient.current.subscribe(`/chess/${code}/${email}`, handleInit);
-      stompClient.current.send(`/app/${code}/${email}/init`, { user: email }, '');
-    });
-  }, []);
-
-  const handleInit = (initMessage: Message) => {
-    const { color, myTurn } = JSON.parse(initMessage.body);
-    setColor(color.toLowerCase());
-    setMyTurn(myTurn);
-  };
-
-  const handleMove = (gameState: Message) => {
-    const { state, game } = JSON.parse(gameState.body);
-
-    switch (state) {
-      case 'Valid move':
-        setMyTurn((prevState) => !prevState);
-        return updateGame(game);
-      case 'Invalid move':
-        openSnackbar({ content: 'Invalid move', severity: 'error' });
-        return game;
-      default:
-        openSnackbar({ content: 'Unkown message', severity: 'error' });
-    }
-  };
-
-  const onDrop: typeof Chessboard.prototype.props.onDrop = ({ sourceSquare, targetSquare }) => {
-    stompClient.current.send(
-      `/app/${code}/makeMove`,
-      {
-        user: email,
-      },
-      JSON.stringify({ from: sourceSquare.toUpperCase(), to: targetSquare.toUpperCase() })
-    );
-  };
   return (
     <Root>
-      {myColor === '' ? (
-        <Loading />
-      ) : (
-        <Chessboard
-          onDrop={onDrop}
-          position={chessboard}
-          draggable={myTurn}
-          allowDrag={({ piece }) => piece[0] === myColor[0]}
-          calcWidth={calcWidth(theme.breakpoints.values)}
-        />
-      )}
+      <Chessboard
+        onDrop={onDrop}
+        position={position}
+        draggable={me.myTurn}
+        squareStyles={squareStyles}
+        onMouseOverSquare={onMouseOverSquare}
+        onMouseOutSquare={onMouseOutSquare}
+        allowDrag={({ piece }) => piece[0] === me.color[0]}
+        calcWidth={calcWidth(theme.breakpoints.values)}
+      />
     </Root>
   );
 };
